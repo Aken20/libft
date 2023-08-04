40a659fdcda42d73c82fb57a221a7be2fd5cd5a9
Aken20 <139094812+Aken20@users.noreply.github.com>
2023-08-04 20:27:13 +0400

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,25 +6,25 @@
 /*   By: ahibrahi <ahibrahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 23:02:49 by ahibrahi          #+#    #+#             */
-/*   Updated: 2023/07/29 00:51:57 by ahibrahi         ###   ########.fr       */
+/*   Updated: 2023/08/04 19:42:11 by ahibrahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_len(const char *str)
+int    ft_len(const char *str)
 {
-	int	len;
+    int    len;
 
-	len = 0; 
-	while (str[len])
-	{
-		len++;
-	}
-	return (len);
+    len = 0; 
+    while (str[len])
+    {
+        len++;
+    }
+    return (len);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char    *ft_strjoin(char const *s1, char const *s2)
 {
 	int		l;
 	int		l1;
@@ -33,8 +33,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	l = ft_len(s1);
 	l1 = ft_len(s2);
-	len = l + l1;
-	sjn = (char *) malloc(len);
+	len = (l + l1);
+	sjn = (char *) malloc(len + 1);
+	if (sjn == 0)
+		return (0);
 	l = 0;
 	while (s1[l])
 	{
@@ -44,8 +46,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	l1 = 0;
 	while (s2[l1])
 	{
-		sjn[l] = s1[l1];
+		sjn[l] = s2[l1];
 		++l1;
+		++l;
 	}
+	sjn[l] = 0;
 	return (sjn);
 }
