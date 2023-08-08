186e0a362c183e0497e2c0a30f80a5c14f0240f2
Aken20 <139094812+Aken20@users.noreply.github.com>
2023-08-08 18:37:21 +0400

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,21 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahibrahi <ahibrahi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aken <aken@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/04 19:48:34 by ahibrahi          #+#    #+#             */
-/*   Updated: 2023/08/04 19:54:55 by ahibrahi         ###   ########.fr       */
+/*   Created: 2023/08/08 09:09:05 by aken              #+#    #+#             */
+/*   Updated: 2023/08/08 09:14:23 by aken             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s1, char const *set)
+static int	is_in_set(char c, const char *set)
 {
-    char *s;
-    
-    s1 = "o00";
-    set = "jj";
-    s = (char *) s1;
-    return (s);
+	while (*set)
+		if (c == *set++)
+			return (0);
+	return (1);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*rtn;
+
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	start = 0;
+	end = 0;
+	while (s1[end])
+		end++;
+	while (is_in_set(s1[start], set) == 0)
+		start++;
+	if (start == end)
+	{
+		if (!(rtn = ft_strdup("")))
+			return (NULL);
+		else
+			return (rtn);
+	}
+	while (is_in_set(s1[end - 1], set) == 0)
+		end--;
+	rtn = ft_substr(s1, start, end - start);
+	return (rtn);
 }
